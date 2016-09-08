b05c62839beb04853531b7f75fb653f6da716d81
rita <ritavepa@gmail.com>
2016-09-08 15:08:57 -0400

    added db callback functions

--- app.js
+++ app.js
@@ -1,59 +1,102 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('test.db');
-var prompt = require('prompt-sync')();
+var db = new sqlite3.Database('tweetDB.db');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.get('/foo', function (req, res) {
-    res.send('yo');
-});
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
-initDB(db);
+function insertTweet(db, user, text) {
+    return new Promise(
+        (resolve, reject) => {
+			db.run("INSERT into tweet VALUES (?, ?)", [user,text]),
+				function (err) {
+					if (err) {
+						reject(err);
+						return;
+					}
+					resolve();
+				}
+			
+        }
+    );
+}
 
-// var fs = require('fs');
-// var os = require('os');
-// function getUser(name) {
-//     return new Promise(
-//         (resolve, reject) => {
-//             fs.getUser(name, function (err, contents) {
-//                 if (err) {
-//                     reject(err);
-//                     return;
-//                 }
-//                 resolve(name);
-//             });
-//         }
-//     );
-// }
-// function writeFile(name, contents) {
-//     return new Promise(
-//         (resolve, reject) => {
-//             fs.writeFile(name, contents, function (err) {
-//                 if (err) {
-//                     reject(err);
-//                     return;
-//                 }
-//                 resolve('success');
-//             });
-//         })
-// }
-// var p = readFile('file.txt', 'utf8');
-// p.then(
-//     (val) => {
-//         var array = val.split("\n");
-//         array.sort();
-//         var success = writeFile('out.txt', array.join(os.EOL));
-//         return success;
-//     }).then(
-//     (val) => {
-//         console.log(val);
-//     }).catch((err) => {
-//         console.log(err);        // handle all possible problems    
-    });
-db.all()
+insertTweet(db, 'John', 'hello').then(
+	(val) => {
+      console.log("inserted");
+	}
+).catch(
+	(err) => {
+     console.log("insert failed:", err);
+	}
+);
+
+
+function deleteTweet(db, user, text) {
+    return new Promise(
+        (resolve, reject) => {
+            var cmd = "DELETE from tweet where col1 = ?";
+			db.run(cmd, user),
+				function (err) {
+					if (err) {
+						reject(err);
+						return;
+					}
+					resolve();
+				}
+			
+        }
+    );
+}
+
+deleteTweet(db, 'John', 'hello').then(
+	(val) => {
+      console.log("deleted");
+	}
+).catch(
+	(err) => {
+     console.log("delete failed:", err);
+	}
+);
+
+//updated function
+
+
+function updateTweet(db, user, text) {
+    return new Promise(
+        (resolve, reject) => {
+            var cmd = "UPDATE tweet SET col2= ? WHERE col1 = ?";
+			db.run(cmd, user, text),
+				function (err) {
+					if (err) {
+						reject(err);
+						return;
+					}
+					resolve();
+				}
+			
+        }
+    );
+}
+
+updateTweet(db, 'john', 'hello').then(
+	(val) => {
+      console.log("updated");
+	}
+).catch(
+	(err) => {
+     console.log("update failed:", err);
+	}
+);
+
+
+
+
+db.all("SELECT * from tweet",function(err,rows){
+	 rows.forEach(function (rows) {
+           console.log(rows);
+     })
+});
+
+
+
+
 db.close();
